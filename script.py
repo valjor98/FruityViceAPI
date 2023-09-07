60cefcb29654bcabaa5fe5489ef30a7785633c0f
valjor98 <valjor98@gmail.com>
2023-09-07 12:59:59 -0600

    Added functionality to save on local DB

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import sqlite3
 
 """
 Python script to fetch data from the FruityVice API.
@@ -19,7 +20,40 @@ def fetch_fruits(url):
         print(f"Error: Received status code {response.status_code}")
         return []
 
-def fetch_and_export_to_csv(filename='fruits.csv', family=None, genus=None, id_range=None):
+# Function to save to SQLite database
+def save_to_db(fruits):
+    conn = sqlite3.connect('fruits.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS fruits
+                      (name TEXT, id INTEGER, family TEXT, genus TEXT, 
+                      order_field TEXT, carbohydrates REAL, protein REAL, fat REAL, 
+                      calories REAL, sugar REAL)''')
+
+    for fruit in fruits:
+        row = (fruit['name'], fruit['id'], fruit['family'], fruit['genus'], fruit['order'], 
+               fruit['nutritions']['carbohydrates'], fruit['nutritions']['protein'], 
+               fruit['nutritions']['fat'], fruit['nutritions']['calories'], fruit['nutritions']['sugar'])
+
+        cursor.execute("INSERT INTO fruits VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+
+    conn.commit()
+    conn.close()
+
+# Function to read from db and print to console
+def read_from_db():
+    conn = sqlite3.connect('fruits.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM fruits")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    conn.close()
+
+def fetch_and_export_to_csv(filename='fruits.csv', family=None, genus=None, id_range=None, to_db=False):
     base_url = 'https://www.fruityvice.com'
     family_fruits = []
     genus_fruits = []
@@ -54,27 +88,31 @@ def fetch_and_export_to_csv(filename='fruits.csv', family=None, genus=None, id_r
     if id_range:
         ids_to_keep = {fruit['id'] for fruit in all_fruits}
         fruits = [fruit for fruit in fruits if fruit['id'] in ids_to_keep]
-        
-    # Export to CSV
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['name', 'id', 'family', 'genus', 'order', 'carbohydrates', 'protein', 'fat', 'calories', 'sugar']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # write the header of the file
 
-        writer.writeheader()
-        for fruit in fruits:
-            row = {
-                'name': fruit['name'],
-                'id': fruit['id'],
-                'family': fruit['family'],
-                'genus': fruit['genus'],
-                'order': fruit['order'],
-                **fruit['nutritions']
-            }
-            writer.writerow(row)
+    if to_db:
+        save_to_db(fruits)
+        read_from_db()
+    else:
+        # Export to CSV
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = ['name', 'id', 'family', 'genus', 'order', 'carbohydrates', 'protein', 'fat', 'calories', 'sugar']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # write the header of the file
+
+            writer.writeheader()
+            for fruit in fruits:
+                row = {
+                    'name': fruit['name'],
+                    'id': fruit['id'],
+                    'family': fruit['family'],
+                    'genus': fruit['genus'],
+                    'order': fruit['order'],
+                    **fruit['nutritions']
+                }
+                writer.writerow(row)
 
 # Examples
 
-# fetch_and_export_to_csv(filename='example1.csv', family='Rosaceae', genus='Malus')
+# fetch_and_export_to_csv(filename='example1.csv', family='Rosaceae', genus='Malus', to_db=True)
 # fetch_and_export_to_csv(filename='example2.csv', family='Rosaceae', id_range=(1, 10))
 # fetch_and_export_to_csv(filename='example3.csv', family='Rosaceae')
 # fetch_and_export_to_csv(filename='example4.csv', genus='Malus')
